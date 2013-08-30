74f02119a9782078321979e2b4ea216beaa03689
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-08-30 06:21:36 +0200

    Start extract sigil.

--- main.rkt
+++ main.rkt
@@ -1,12 +1,22 @@
 #lang racket
 
-(define open-tag "{{")
-(define close-tag "}}")
+(struct token (sigil content section))
 
-(struct token (state content))
-
-(define (parse-template mustache-file)
+(define (parse-template mustache-file [open-tag "{{"] [close-tag "}}"])
   (define template (open-input-file mustache-file))
+  (define state-pattern
+    (pregexp
+     (string-append
+      "^"
+      "\\s*"                            ; Skip any whitespace
+      "(#|\\^|/|=|!|<|>|&|\\{)?"        ; Check for a tag type and capture it
+      "\\s*"                            ; Skip any whitespace
+      "([^(?:\\}?"
+        (regexp-quote close-tag) ")]+)" ; Capture the text inside of the tag
+      "\\s*"                            ; Skip any whitespace
+      "\\}?"                            ; Skip balancing '}' if it exists
+      "(.*)$")))                        ; Capture the rest of the string
+
 
   (define (scan-static tokens)
     (define open-tag-pos
@@ -14,13 +24,13 @@
 
     (cond
         [(not open-tag-pos)
-         (parse 'end (append tokens (list (token 'static (port->string template)))))]
+         (parse 'end (append tokens (list (token 'static (port->string template) empty))))]
         [else
          (define content-length (car (first open-tag-pos)))
          (define content (make-string content-length))
 
          (read-string! content template 0 content-length)
-         (parse 'tag (append tokens (list (token 'static content))))]))
+         (parse 'tag (append tokens (list (token 'static content empty))))]))
 
   (define (scan-tag tokens)
     (define close-tag-pos
@@ -34,17 +44,51 @@
     (void (read-string (string-length open-tag) template))
     (read-string! content template 0 content-length)
     (void (read-string (string-length close-tag) template))
-    (parse 'static (append tokens (list (token 'etag content)))))
 
+    (define l (regexp-match state-pattern content))
+    (define sigil (second l))
+    (define value (third l))
+
+    (case sigil
+      [(#f)
+       (parse 'static (append tokens (list (token 'etag value empty))))]
+      [(">" "<")
+       (parse 'static (append tokens (list (token 'partial value) empty)))]
+      [("{" "&")
+       (parse 'static (append tokens (list (token 'utag value empty))))]
+      [("!")
+       (parse 'static tokens)]
+      [("#")
+       (parse 'static (append tokens
+                              (list (token 'section
+                                           value
+                                           (parse 'static empty)))))]
+      [("^")
+       (parse 'static (append tokens
+                              (list (token 'inverted-section
+                                           value
+                                           (parse 'static empty)))))]
+      [("/") tokens]))
 
   ; parse: content tokens -> tokens
   (define (parse state tokens)
     (cond
       [(eq? state 'static) (scan-static tokens)]
       [(eq? state 'tag) (scan-tag tokens)]
-      [else tokens]))
+      [else
+       (when (not (port-closed? template)) (close-input-port template))
+       tokens]))
 
   (parse 'static empty))
 
 (define (display-token token)
-  (format "state: ~a, content: ~a" (token-state token) (token-content token)))
+  (displayln (format "sigil: ~a, content: ~a"
+                     (token-sigil token)
+                     (token-content token)))
+
+  (when (eq? (token-sigil token) 'section)
+    (displayln (format "*** Section ~a ***" (token-content token)))
+    (map display-token (token-section token))
+    (displayln "*****************")))
+
+(provide (all-defined-out))
--- test/test.rkt
+++ test/test.rkt
@@ -1,29 +1,13 @@
 #lang racket
 
-(require rackunit)
 (require "../main.rkt")
 
-;; check-good-render?:
-;;   mustache-render  mustache-file musatche-data-file rendered-file
-;;   -> doesn't return
-;; Check that the mustache-renderer rendes correctly mustache-file
-;; using mustache-data-file. The mustache-renderer production will be
-;; equivalent to rendered-file.
-(define (check-good-render?
-         mustache-renderer mustache-file mustache-data-file rendered-file)
-  (with-handlers ([exn:fail:filesystem? (lambda (v) (fail v))])
-  (check-equal?
-     (mustache-renderer mustache-file (load mustache-data-file))
-     (port->string (open-input-file rendered-file))
-     (format "Error on ~a test. Find test at examples/~a.*"
-             mustache-file mustache-file))))
 
 (define bench-list (list "boolean"
                          "carriage_return"
                          "comments"
                          "complex"
                          "deep_partial"
-                         "delimiters"
                          "dot_notation"
                          "error_not_found"
                          "escaped"
@@ -37,17 +21,14 @@
                          "tenthousand"
                          "twice"
                          "two_in_a_row"
-                         "unascaped"
+                         "unescaped"
 ))
 
 (void (map
  (lambda (bench-id)
-   (define mustache-file (string-append "examples/" bench-id ".html"))
-   (define mustache-data-file (string-append "examples/" bench-id ".rkt"))
-   (define rendered-file (string-append "examples/" bench-id ".txt"))
+   (define template-file (string-append "test/examples/" bench-id ".html"))
+   (define tokens (parse-template template-file))
 
-   (check-good-render? render-file
-                       mustache-file
-                       mustache-data-file
-                       rendered-file))
+   (displayln (format "== Tokens for ~a==" bench-id))
+   (map display-token tokens))
  bench-list))
