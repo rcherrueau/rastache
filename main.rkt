c5f7325a149a8724b15e55cf43b2c765bf2dbc4a
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-08-30 15:41:00 +0200

    Spread open-tag and close-tag
    Spread open-tag and close-tag for a futur support of set delimiter feature

--- main.rkt
+++ main.rkt
@@ -2,7 +2,7 @@
 
 (struct token (sigil content section))
 
-(define (parse-template mustache-file [open-tag "{{"] [close-tag "}}"])
+(define (mustache-tokenize mustache-file [open-tag "{{"] [close-tag "}}"])
   (define template (open-input-file mustache-file))
   (define state-pattern
     (pregexp
@@ -18,21 +18,29 @@
       "(.*)$")))                        ; Capture the rest of the string
 
 
-  (define (scan-static tokens)
+  (define (scan-static tokens otag ctag)
     (define open-tag-pos
       (regexp-match-peek-positions open-tag template))
 
     (cond
         [(not open-tag-pos)
-         (parse 'end (append tokens (list (token 'static (port->string template) empty))))]
+         (scan 'end
+                (append tokens (list (token 'static
+                                            (port->string template)
+                                            empty)))
+                otag
+                ctag)]
         [else
          (define content-length (car (first open-tag-pos)))
          (define content (make-string content-length))
 
          (read-string! content template 0 content-length)
-         (parse 'tag (append tokens (list (token 'static content empty))))]))
+         (scan 'tag
+                (append tokens (list (token 'static content empty)))
+                otag
+                ctag)]))
 
-  (define (scan-tag tokens)
+  (define (scan-tag tokens otag ctag)
     (define close-tag-pos
       (regexp-match-peek-positions close-tag template))
 
@@ -51,35 +59,50 @@
 
     (case sigil
       [(#f)
-       (parse 'static (append tokens (list (token 'etag value empty))))]
+       (scan 'static
+              (append tokens (list (token 'etag value empty)))
+              otag
+              ctag)]
       [(">" "<")
-       (parse 'static (append tokens (list (token 'partial value) empty)))]
+       (scan 'static
+              (append tokens (list (token 'partial value) empty))
+              otag
+              ctag)]
       [("{" "&")
-       (parse 'static (append tokens (list (token 'utag value empty))))]
+       (scan 'static
+              (append tokens (list (token 'utag value empty)))
+              otag
+              ctag)]
       [("!")
-       (parse 'static tokens)]
+       (scan 'static tokens otag ctag)]
       [("#")
-       (parse 'static (append tokens
-                              (list (token 'section
-                                           value
-                                           (parse 'static empty)))))]
+       (scan 'static
+              (append tokens
+                      (list (token 'section
+                                   value
+                                   (scan 'static empty otag ctag))))
+              otag
+              ctag)]
       [("^")
-       (parse 'static (append tokens
-                              (list (token 'inverted-section
-                                           value
-                                           (parse 'static empty)))))]
+       (scan 'static
+              (append tokens
+                      (list (token 'inverted-section
+                                   value
+                                   (scan 'static empty otag ctag))))
+              otag
+              ctag)]
       [("/") tokens]))
 
-  ; parse: content tokens -> tokens
-  (define (parse state tokens)
+  ; scan: content tokens -> tokens
+  (define (scan state tokens otag ctag)
     (cond
-      [(eq? state 'static) (scan-static tokens)]
-      [(eq? state 'tag) (scan-tag tokens)]
+      [(eq? state 'static) (scan-static tokens otag ctag)]
+      [(eq? state 'tag) (scan-tag tokens otag ctag)]
       [else
        (when (not (port-closed? template)) (close-input-port template))
        tokens]))
 
-  (parse 'static empty))
+  (scan 'static empty open-tag close-tag))
 
 (define (display-token token)
   (displayln (format "sigil: ~a, content: ~a"
