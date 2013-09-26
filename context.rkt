7bb149493fce7f7abf721e9701ee291aba625905
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-09-26 13:36:26 +0200

    Context maker produces required defines.
    Currently, it only produces defines for the first level of mustache
    object.

--- context.rkt
+++ context.rkt
@@ -1,14 +1,27 @@
 #lang racket
 
-(define (exist-define define-name define-list)
-  (member define-name define-list))
+;; (define (exist-define define-name define-list)
+;;   (member define-name define-list))
 
 (define-syntax (mustache-ctx x)
   (syntax-case x ()
-    [(_ '((k1 v1 ...) (k2 v2 ...) ...))
-     #'(make-immutable-hash (list (mustache-data (quote (k1 v1 ...)))
-                                  (mustache-data (quote (k2 v2 ...)))
-                                  ...))]))
+    [(_ name '((k1 v1 ...) (k2 v2 ...) ...))
+     (let ([make-id
+            (lambda (template . ids)
+              (let ([str (apply format template (map syntax->datum ids))])
+                (datum->syntax x (string->symbol str))))])
+
+     (with-syntax ([(mustache-key ...)
+                    (map (lambda (f) (make-id "mustache-~a"  f))
+                         (syntax->list #'(k1 k2 ...)))]
+                   [(key ...) #'(k1 k2 ...)])
+       #'(begin
+           (define (mustache-key ctx) (hash-ref ctx 'key))
+           ...
+           (define name (make-immutable-hash
+                         (list (mustache-data (quote (k1 v1 ...)))
+                               (mustache-data (quote (k2 v2 ...)))
+                               ...))))))]))
 
 (define-syntax (mustache-data x)
   (syntax-case x ()
@@ -27,7 +40,6 @@
     [(_ val1 val2 ...)
      #'(list (mustache-val val1) (mustache-val val2) ...)]))
 
-
 ;; Following not work :
 ;; (let ([ctx2 '((name "Jim" "Ronan") (age 24) (admin #t))])
 ;;   (mustache-ctx ctx-2))
@@ -37,8 +49,8 @@
 (define (self-item ctx) (ctx))
 
 ; (mustache-ctx '((name "Jim") (age 24) (admin #t)))
-(define hash-test
-  (mustache-ctx
+;(define hash-test
+  (mustache-ctx ctx
 
    ;; CTX-1
     ;; '((name "Foo1" "Foo2" "Foo3" "Foo4") (age 24) (admin #t)
@@ -48,9 +60,7 @@
       (item '((name "red") (current #t) (url "#Red"))
             '((name "green") (current #f) (url "#Green"))
             '((name "blue") (current #f) (url "#Blue")))
-      (link (lambda (self) (not (eq? (self-current self) #t))))
-      (list (lambda (self) (not (eq? (length (self-item self)) 0))))
-      (empty (lambda (self) (eq? (length (self-item self)) 0)))
-)))
-
-hash-test
+      (link (lambda (self) (not (eq? (mustache-current self) #t))))
+      (list (lambda (self) (not (eq? (length (mustache-item self)) 0))))
+      (empty (lambda (self) (eq? (length (mustache-item self)) 0)))
+))
