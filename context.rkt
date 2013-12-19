384ee9473e12965368033a5058e5eceeabfdb20c
Ronan Cherrueau <Ronancherrueau@gmail.com>
2013-12-19 17:27:42 +0100

    Macro for mustache context

--- context.rkt
+++ context.rkt
@@ -70,14 +70,19 @@
                 make-the-define
                 values ... . tail))]))]))
 
-(mustache-make-defines
- '((header (lambda () "Colors"))
-   (link (lambda (self) (not (eq? (mustache-current self) #t))))
-   (list (lambda (self) (not (eq? (length (mustache-item self)) 0))))
-   (item '((name "red") (current #t) (url "#Red"))
-         '((name "green") (current #f) (url "#Green"))
-         '((name "blue") (current #f) (url "#Blue")))
-   (empty (lambda (self) (eq? (length (mustache-item self)) 0)))))
+;; Constructs the hash-table for a specific mustache-expr.
+(define-syntax (mustache-make-htable x)
+  (syntax-case x ()
+    ;; many mustache-data(s) in a mustache-expr
+    [(_ '([key1 val1 ...] [key2 val2 ...] ...))
+     #'(make-immutable-hash
+        (list (cons 'key1 (mustache-make-val val1 ...))
+              (cons 'key2 (mustache-make-val val2 ...))
+              ...))]
+    ;; One unique mustache-data in a mustache-expr
+    [(_ '(key val ...))
+     #'(make-immutable-hash
+        (list (cons 'key (mustache-make-val val ...))))]))
 
 
 ;; Constructs the value for the val part of a mustache-data.
@@ -95,210 +100,61 @@
              (mustache-make-val v2)
              ...)]))
 
-(mustache-make-val "foo")
-;; > "foo"
+;; Constructs the mustache context for a mustech-expr. Two parts
+;; compose the context. The first part is an hash-table that indexes
+;; all elements. The second part is a collection of accessors to
+;; access to each elements of the hash-table.
+(define-syntax (mustache-make-ctx x)
+  (syntax-case x ()
+    [(_ ctx-name '(kv ...))
+     #'(begin
+         (mustache-make-defines '(kv ...))
+         (define ctx-name (mustache-make-htable '(kv ...))))]))
 
-(mustache-make-val '42 '43 '44)
-;; '(42 43 44)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Tests
 
-((mustache-make-val (lambda (self) self)) '42)
-;; > 42
+;; (mustache-make-ctx
+;;  ctx
+;;  '((header (lambda () "Colors"))
+;;    (link (lambda (self) (not (eq? (mustache-current self) #t))))
+;;    (list (lambda (self) (not (eq? (length (mustache-item self)) 0))))
+;;    (item '((name "red") (current #t) (url "#Red"))
+;;          '((name "green") (current #f) (url "#Green"))
+;;          '((name "blue") (current #f) (url "#Blue")))
+;;    (empty (lambda (self) (eq? (length (mustache-item self)) 0)))))
 
-;; (mustache-make-val
-;;  '((name "red") (current #t) (url "#Red"))
-;;  '((name "green") (current #f) (url "#Green"))
-;;  '((name "blue") (current #f) (url "#Blue")))
-;; > '(#hash((name . "red") (current . #t) (url . "#Red"))
-;;     #hash((name . "green") (current . #f) (url . "#Green"))
-;;     #hash((name . "blue") (current . #f) (url . "#Blue")))
+;; ((mustache-header ctx))
 
+;; (for ([inner-ctx (mustache-item ctx)])
+;;   (printf "~s ~s ~s~n"
+;;           (mustache-name inner-ctx)
+;;           (mustache-current inner-ctx)
+;;           (mustache-url inner-ctx)))
 
+;; (mustache-make-defines
+;;  '((header (lambda () "Colors"))
+;;    (link (lambda (self) (not (eq? (mustache-current self) #t))))
+;;    (list (lambda (self) (not (eq? (length (mustache-item self)) 0))))
+;;    (item '((name "red") (current #t) (url "#Red"))
+;;          '((name "green") (current #f) (url "#Green"))
+;;          '((name "blue") (current #f) (url "#Blue")))
+;;    (empty (lambda (self) (eq? (length (mustache-item self)) 0)))))
 
-;; (define-syntax (mustache-make-htable x)
-;;   (syntax-case x ()
-;;     [(_ '([key vals] ...))
-;;      (with-syntax
-;;          ([(values ...)
-;;            (map (lambda (k v)
-;;                   (syntax-case #'v ()
-;;                     (
-;;                 (syntax->list #'([key vals] ...)))
-;;      #'(make-immutable-hash (list values ...))]
+;; (define ctx
+;;   (mustache-make-htable
+;;    '((header (lambda () "Colors"))
+;;      (link (lambda (self) (not (eq? (mustache-current self) #t))))
+;;      (list (lambda (self) (not (eq? (length (mustache-item self)) 0))))
+;;      (item '((name "red") (current #t) (url "#Red"))
+;;            '((name "green") (current #f) (url "#Green"))
+;;            '((name "blue") (current #f) (url "#Blue")))
+;;      (empty (lambda (self) (eq? (length (mustache-item self)) 0))))))
 
+;; (mustache-make-val "foo")
+;; ;; > "foo"
 
+;; (mustache-make-val '42 '43 '44)
+;; ;; '(42 43 44)
 
-;; (define (mustache-name c) c)
-
-;
-;(define (la x) x)
-;
-;(syntax-original? #'la)
-
-
-;; (define (exist-define define-name define-list)
-;;   (member define-name define-list))
-
-
-;; http://stackoverflow.com/a/2890557
-
-; (null? (cdr (syntax-e #'((m m m m m m) (d d d d d)))))
-; (null? (cdr (syntax-e #'((m m m m m m)))))
-
-;; (define-syntax (mustache-ctx x)
-;;   (struct mdc (defines contexts))
-
-;;   ; Create an datum from a template readable with `format'.
-;;   (define (make-id template . ids)
-;;     (define str (apply format template (map syntax->datum ids)))
-;;     (datum->syntax x (string->symbol str)))
-
-;;   (define (has-definitions? m-data)
-;;     (not (null? (cdr (syntax-e m-data)))))
-
-;;   (define (extract-defs-ctx m-data mdc)
-;;     (cond
-;;      [(has-definitions? m-data)
-;;       (let* ([m-data-e (syntax-e m-data)]
-;;              [new-ctx (car m-data-e)]
-;;              [new-defs (cadr m-data-e)]
-;;              [old-ctx (mdc-ctx
-
-
-
-;;   (define (extract-defs-ctxs m-datas)
-;;     (define m-defs-ctxs
-;;       (foldl extract-defs-ctx (mdc (begin) (begin)) m-datas))
-
-;;     #`(begin
-;;         #,(mdc-defines m-defs-ctxs)
-;;         #,(mdc-contextes m-defs-ctxs)))
-
-;;   (syntax-case x ()
-;;     [(_ name '((k1 v1 ...) (k2 v2 ...) ...))
-;;      (with-syntax ([(key ...) #'(k1 k2 ...)]
-;;                    [(mustache-key ...)
-;;                     (map (lambda (f) (make-id "mustache-~a" f))
-;;                          (syntax->list #'(k1 k2 ...)))]
-;;                    [(mustache-define-and-mustache-ctx)
-;;                     (lala (syntax->list #'((mustache-data (quote (k1 v1 ...)))
-;;                                            (mustache-data (quote (k2 v2 ...)))
-;;                                            ...)))])
-;;        #'(begin
-;;            (begin
-;;              (define (mustache-key ctx) (hash-ref ctx 'key))
-;;              ...)
-;;            (begin
-;;              (define name (make-immutable-hash
-;;                          (list (mustache-data (quote (k1 v1 ...)))
-;;                                (mustache-data (quote (k2 v2 ...)))
-;;                                ...))))))]))
-;;           ;; (mustache-define-and-mustache-ctx)))))])))
-
-;; (define-syntax (mustache-data x)
-;;   (syntax-case x ()
-;;     [(_ (quote (key val ...)))
-;;      #'(cons (quote key) (mustache-val val ...))]))
-
-;; (define-syntax (mustache-val x)
-;;   (syntax-case x ()
-;;     [(_ '((k1 v1 ...) (k2 v2 ...) ...))
-;;      #'(make-immutable-hash (list (mustache-data (quote (k1 v1 ...)))
-;;                                   (mustache-data (quote (k2 v2 ...)))
-;;                                   ...))]
-
-;;     [(_ val)
-;;      #'val]
-;;     [(_ val1 val2 ...)
-;;      #'(list (mustache-val val1) (mustache-val val2) ...)]))
-
-;; ;; Following not work :
-;; ;; (let ([ctx2 '((name "Jim" "Ronan") (age 24) (admin #t))])
-;; ;;   (mustache-ctx ctx-2))
-
-
-;; ;; FIXME: generate defines for nested mustache
-;; (define (mustache-current ctx) (hash-ref ctx 'current))
-;; (define (mustache-name ctx) (hash-ref ctx 'name))
-;; (define (mustache-url ctx) (hash-ref ctx 'url))
-
-
-; (mustache-ctx ctx '((name "Jim") (age 24) (admin #t)))
-;; (mustache-ctx ctx
-
-;;    ;; CTX-1
-;;     ;; '((name "Foo1" "Foo2" "Foo3" "Foo4") (age 24) (admin #t)
-
-;;   ;; CTX-2
-;;     '((header (lambda () "Colors"))
-;;       (item '((name "red") (current #t) (url "#Red"))
-;;             '((name "green") (current #f) (url "#Green"))
-;;             '((name "blue") (current #f) (url "#Blue")))
-;;       (link (lambda (self) (not (eq? (mustache-current self) #t))))
-;;       (list (lambda (self) (not (eq? (length (mustache-item self)) 0))))
-;;       (empty (lambda (self) (eq? (length (mustache-item self)) 0)))
-;; ))
-
-;; (define-syntax (mustache-ctx x)
-;;   (syntax-case x ()
-;;     [(_ name '((k1 v1 ...) (k2 v2 ...) ...))
-;;      (let ([make-id
-;;             (lambda (template . ids)
-;;               (let ([str (apply format template (map syntax->datum ids))])
-;;                 (datum->syntax x (string->symbol str))))])
-
-;;      (with-syntax ([(mustache-key ...)
-;;                     (map (lambda (f) (make-id "mustache-~a"  f))
-;;                          (syntax->list #'(k1 k2 ...)))]
-;;                    [(key ...) #'(k1 k2 ...)])
-;;        #'(begin
-;;            (define (mustache-key ctx) (hash-ref ctx 'key))
-;;            ...
-;;            (define name (make-immutable-hash
-;;                          (list (mustache-data (quote (k1 v1 ...)))
-;;                                (mustache-data (quote (k2 v2 ...)))
-;;                                ...))))))]))
-
-;; (define-syntax (mustache-data x)
-;;   (syntax-case x ()
-;;     [(_ (quote (key val ...)))
-;;      #'(cons (quote key) (mustache-val val ...))]))
-
-;; (define-syntax (mustache-val x)
-;;   (syntax-case x ()
-;;     [(_ '((k1 v1 ...) (k2 v2 ...) ...))
-;;      #'(make-immutable-hash (list (mustache-data (quote (k1 v1 ...)))
-;;                                   (mustache-data (quote (k2 v2 ...)))
-;;                                   ...))]
-
-;;     [(_ val)
-;;      #'val]
-;;     [(_ val1 val2 ...)
-;;      #'(list (mustache-val val1) (mustache-val val2) ...)]))
-
-;; ;; Following not work :
-;; ;; (let ([ctx2 '((name "Jim" "Ronan") (age 24) (admin #t))])
-;; ;;   (mustache-ctx ctx-2))
-
-
-;; ;; FIXME: generate defines for nested mustache
-;; (define (mustache-current ctx) (ctx))
-;; (define (mustache-name ctx) (ctx))
-;; (define (mustache-url ctx) (ctx))
-
-
-;; ; (mustache-ctx '((name "Jim") (age 24) (admin #t)))
-;; ;(define hash-test
-;;   (mustache-ctx ctx
-
-;;    ;; CTX-1
-;;     ;; '((name "Foo1" "Foo2" "Foo3" "Foo4") (age 24) (admin #t)
-
-;;   ;; CTX-2
-;;     '((header (lambda () "Colors"))
-;;       (item '((name "red") (current #t) (url "#Red"))
-;;             '((name "green") (current #f) (url "#Green"))
-;;             '((name "blue") (current #f) (url "#Blue")))
-;;       (link (lambda (self) (not (eq? (mustache-current self) #t))))
-;;       (list (lambda (self) (not (eq? (length (mustache-item self)) 0))))
-;;       (empty (lambda (self) (eq? (length (mustache-item self)) 0)))
-;; ))
+;; ((mustache-make-val (lambda (self) self)) '42)
+;; ;; > 42
