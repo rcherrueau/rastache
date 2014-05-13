eda0e09c7a7624be93076c45926931c55033c263
Rl3x Fish <ronancherrueau@gmail.com>
2014-05-13 02:25:44 +0200

    Add token-mock managment in tests

--- tests/rastache-test-case.rkt
+++ tests/rastache-test-case.rkt
@@ -1,14 +1,19 @@
 #lang racket/base
 
 (require rackunit
+         racket/format
+         "../scanner.rkt"
+         "../renderer.rkt"
          "../rastache.rkt"
-         (for-syntax racket/base))
+         (for-syntax racket))
 
 (provide (all-defined-out))
 
 ; Rastache test case
 (define-syntax (rast-t-case stx)
   (syntax-case stx ()
+    ;; rast-t-case: test-name context template expected fail-msg
+    ;; expended to rackunit test case.
     [(_ t-name hash t-template t-expected t-error-msg)
      #'(test-case
         t-name
@@ -18,4 +23,29 @@
           (rastache-render tokens hash rendered)
           (check-equal? (get-output-string rendered)
                         expected
-                        t-error-msg)))]))
+                        t-error-msg)))]
+
+    [(_ t-name hash t-template t-expected t-mock-token t-error-msg)
+     (let ([make-name (λ (type syntax)
+                         (format "~a -- ~a"
+                                 type
+                                 (syntax->datum syntax)))])
+       (with-syntax
+           ([scanner-t-name (make-name "SCANNER" #'t-name)]
+            [scanner-t-error-msg (make-name "SCANNER" #'t-error-msg)]
+            [render-t-name (make-name "RENDER" #'t-name)]
+            [render-t-error-msg (make-name "RENDER" #'t-error-msg)])
+         #'(begin
+             (test-case
+              scanner-t-name
+              (let ([template (open-input-string t-template)])
+                (check-equal? (~a (tokenize template))
+                              (~a t-mock-token)
+                              scanner-t-error-msg)))
+             (test-case
+              render-t-name
+              (let ([rendered (open-output-string)])
+                (render t-mock-token hash rendered)
+                (check-equal? (get-output-string rendered)
+                              t-expected
+                              render-t-error-msg))))))]))
